d80552c511566a497e579ebb1693fa5118db4d7b
Subhra Shaw <professionalsubhra70@gmail.com>
2025-02-03 07:13:01 +0000

    conversion of datatype part1 completed

---
+++ basics/conversionDatatypes.js
@@ -0,0 +1,27 @@
+// Other types to integer
+// let var1 = undefined
+// let intVal = Number(var1);
+// console.log(typeof var1);
+// console.log(typeof intVal);
+// console.log(intVal);
+
+/* 
+"45" -> 45
+"subhra" -> NaN
+null -> 0
+false -> 0 true -> 1
+undefined -> NaN
+*/
+
+// other types to boolean
+let var2 = null
+let boolVal = Boolean(var2);
+console.log(typeof var2);
+console.log(typeof boolVal);
+console.log(boolVal);
+/*
+0 -> false 1 -> true
+"" -> false
+"hii" -> true
+null -> false
+*/
