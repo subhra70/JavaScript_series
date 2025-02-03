eceb825975522c0148d8d1d4c6b1d77f56912216
Subhra Shaw <professionalsubhra70@gmail.com>
2025-02-03 06:55:57 +0000

    datatypes completed

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+let name="Subhra Shaw" // String datatype
+let age=21 // int datatype
+/*
+Data Types:
+string
+int
+bigInt -> for large integer data
+boolean
+symbol -> unique vaalue
+null -> empty
+undefined -> value not defined
+
+object
+*/
+let var1 // undefined 
+let var2=null //null
+console.log(var1)
+console.log(var2)
+
+// typeof
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
