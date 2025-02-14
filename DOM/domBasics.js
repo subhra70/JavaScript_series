8b7eafa5019b5227f3c4d95046a437facb3f6cf9
Subhra704 <shawsubhra68@gmail.com>
2025-02-14 23:14:24 +0530

    learned creation of element

---
+++ DOM/domBasics.js
@@ -0,0 +1,8 @@
+// Creating node in javascript
+const div=document.createElement('div');
+div.setAttribute('id','card')
+div.setAttribute('class','card')
+div.style.backgroundColor='yellow'
+div.style.padding='20px'
+div.style.borderRadius='5px'
+document.body.appendChild(div)
